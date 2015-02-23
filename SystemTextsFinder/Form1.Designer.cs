255abda1679b9eba95a003f403dbc13858c63dcf
Per Jahn <per.jahn@wipcore.se>
2015-02-23 15:07:30 +0100

    Added tool: SystemTextsFinder

---
+++ SystemTextsFinder/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace SystemTextsFinder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.buttonOpenFile = new System.Windows.Forms.Button();
+            this.buttonCheckIfUsed = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 41);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(1562, 933);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "SystemTexts.xml";
+            // 
+            // buttonOpenFile
+            // 
+            this.buttonOpenFile.Location = new System.Drawing.Point(12, 12);
+            this.buttonOpenFile.Name = "buttonOpenFile";
+            this.buttonOpenFile.Size = new System.Drawing.Size(100, 23);
+            this.buttonOpenFile.TabIndex = 0;
+            this.buttonOpenFile.Text = "Open file...";
+            this.buttonOpenFile.UseVisualStyleBackColor = true;
+            this.buttonOpenFile.Click += new System.EventHandler(this.buttonOpenFile_Click);
+            // 
+            // buttonCheckIfUsed
+            // 
+            this.buttonCheckIfUsed.Location = new System.Drawing.Point(237, 12);
+            this.buttonCheckIfUsed.Name = "buttonCheckIfUsed";
+            this.buttonCheckIfUsed.Size = new System.Drawing.Size(100, 23);
+            this.buttonCheckIfUsed.TabIndex = 1;
+            this.buttonCheckIfUsed.Text = "Check if used";
+            this.buttonCheckIfUsed.UseVisualStyleBackColor = true;
+            this.buttonCheckIfUsed.Click += new System.EventHandler(this.buttonCheckIfUsed_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(343, 14);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(500, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "C:\\Git\\onemed_release\\Src";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1586, 986);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.buttonCheckIfUsed);
+            this.Controls.Add(this.buttonOpenFile);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SystemTextsFinder";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button buttonOpenFile;
+        private System.Windows.Forms.Button buttonCheckIfUsed;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ SystemTextsFinder/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace SystemTextsFinder
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+        }
+
+        private void buttonOpenFile_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() != System.Windows.Forms.DialogResult.Cancel)
+            {
+                string filename = openFileDialog1.FileName;
+                LoadFile(filename);
+            }
+        }
+
+        void LoadFile(string filename)
+        {
+            XDocument xdoc = XDocument.Load(filename);
+
+            IEnumerable<string> languages = xdoc
+                .Descendants("Name")
+                .Where(el => el.Attribute("lang") != null)
+                .Select(el => el.Attribute("lang").Value)
+                .Distinct()
+                .OrderBy(t => t);
+
+            dataGridView1.Columns.Clear();
+
+            var texts = xdoc
+                .Descendants("EnovaSystemText")
+                .OrderBy(el => el.Element("Identifier").Value);
+
+            DataTable dt = new DataTable();
+
+            dt.Columns.Add("Identifier");
+            foreach (string lang in languages)
+            {
+                dt.Columns.Add(lang);
+            }
+
+
+            foreach (var text in texts)
+            {
+                DataRow dr = dt.NewRow();
+                dr["Identifier"] = text.Element("Identifier").Value;
+                foreach (var lang in text.Elements("Name"))
+                {
+                    string l = lang.Attribute("lang").Value;
+                    dr[l] = lang.Value;
+                }
+                dt.Rows.Add(dr);
+            }
+
+            dataGridView1.DataSource = dt;
+
+            dataGridView1.Columns["Identifier"].Width = 250;
+            foreach (string lang in languages)
+            {
+                dataGridView1.Columns[lang].Width = 250;
+            }
+
+            return;
+        }
+
+        private void buttonCheckIfUsed_Click(object sender, EventArgs e)
+        {
+            Dictionary<string, string> foundIdentifiers = new Dictionary<string, string>();
+
+            DataTable dt = (DataTable)dataGridView1.DataSource;
+            if (dt != null)
+            {
+                IEnumerable<string> identifiers = dt.AsEnumerable().Select(r => (string)r["Identifier"]);
+
+                string path = textBox1.Text;
+
+                string[] sourcefiles =
+                    Directory.GetFiles(path, "*.cs", SearchOption.AllDirectories)
+                    .Union(Directory.GetFiles(path, "*.as?x", SearchOption.AllDirectories))
+                    .ToArray();
+
+                if (MessageBox.Show("Searching " + sourcefiles.Length + " files...", "Parse files", MessageBoxButtons.OKCancel) != System.Windows.Forms.DialogResult.OK)
+                {
+                    return;
+                }
+
+                long rowcount = 0;
+                long matches = 0;
+
+                foreach (string sourcefile in sourcefiles)
+                {
+                    string[] rows = File.ReadAllLines(sourcefile);
+                    rowcount += rows.Length;
+
+                    foreach (string row in rows)
+                    {
+                        foreach (string identifier in identifiers)
+                        {
+                            if (row.Contains(identifier))
+                            {
+                                if (foundIdentifiers.ContainsKey(identifier))
+                                {
+                                    foundIdentifiers[identifier] += ", " + sourcefile;
+                                }
+                                else
+                                {
+                                    foundIdentifiers[identifier] = sourcefile;
+                                }
+                                matches++;
+                            }
+                        }
+                    }
+                }
+
+                if (!dt.Columns.Contains("InSourceCode"))
+                {
+                    dt.Columns.Add("InSourceCode");
+                }
+
+                int foundIdentifiersCount = 0;
+
+                foreach (DataRow dr in dt.Rows)
+                {
+                    string identifier = (string)dr["Identifier"];
+                    if (foundIdentifiers.ContainsKey(identifier))
+                    {
+                        dr["InSourceCode"] = foundIdentifiers[identifier];
+                        foundIdentifiersCount++;
+                    }
+                    else
+                    {
+                        dr["InSourceCode"] = null;
+                    }
+                }
+
+                MessageBox.Show(
+                    "Rowcount: " + rowcount + Environment.NewLine +
+                    "Matches: " + matches + Environment.NewLine +
+                    "Missing identifiers: " + (dt.Rows.Count - foundIdentifiersCount) + "/" + dt.Rows.Count);
+            }
+        }
+    }
+}
